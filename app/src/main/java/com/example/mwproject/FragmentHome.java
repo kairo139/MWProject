e8e596cedea5e3bf2f7a2451c6196840d687e2a5
wjy <wjy308@nate.com>
2020-06-12 22:10:56 +0900

    드디어 틀 만들었네 싀바

---
+++ app/src/main/java/com/example/mwproject/FragmentHome.java
@@ -0,0 +1,21 @@
+package com.example.mwproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+public class FragmentHome extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/com/example/mwproject/MainActivity.java
+++ app/src/main/java/com/example/mwproject/MainActivity.java
@@ -1,19 +1,72 @@
 package com.example.mwproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentRanking fragmentRanking = new FragmentRanking();
+    private FragmentCategory fragmentCategory = new FragmentCategory();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //툴바
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //사이드 바
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,0,0);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //바텀 네비게이션 (fragment)
+        FragmentTransaction transaction = fragmentManager.beginTransaction(); //맨 처음에 나타날 frameLayout 설정
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.home:
+                    transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+                    break;
+                case R.id.ranking:
+                    transaction.replace(R.id.frameLayout, fragmentRanking).commitAllowingStateLoss();
+                    break;
+                case R.id.category:
+                    transaction.replace(R.id.frameLayout, fragmentCategory).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 }
