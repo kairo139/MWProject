5a99d7b337afdeee47435580255017b1e94cd0ad
kairo139 <kairo139@naver.com>
2020-06-16 22:03:30 +0900

    2020.06.16 개인정보 설정 자바 추가

--- app/src/main/java/com/example/mwproject/MainActivity.java
+++ app/src/main/java/com/example/mwproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mwproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button go;
+    public static final int sub = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        go = findViewById(R.id.go);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ProfileChange.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mwproject/ProfileChange.java
@@ -0,0 +1,17 @@
+package com.example.mwproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profilechange);
+
+    }
+}
