04c02a7db4d95b98c4167cce770ea2fbffb93a65
kairo139 <kairo139@naver.com>
2020-06-18 21:19:19 +0900

    2020.06.17

--- app/src/main/java/com/example/mwproject/Player.java
+++ app/src/main/java/com/example/mwproject/Player.java
@@ -6,11 +6,21 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Player extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Player extends YouTubeBaseActivity {
+
+    YouTubePlayerView youtubeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
+
+        youtubeView = (YouTubePlayerView)findViewById(R.id.youtubeplayer);
+        
+
     }
 }
 
