c9eafdda596b371e2f2035f85722ec329da28c80
wjy <wjy308@nate.com>
2020-06-18 13:35:47 +0900

    보관함 ㅅ

--- app/src/main/java/com/example/mwproject/FragmentCategory.java
+++ app/src/main/java/com/example/mwproject/FragmentCategory.java
@@ -13,6 +13,6 @@ public class FragmentCategory extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        return inflater.inflate(R.layout.genre, container, false);
     }
 }
--- app/src/main/java/com/example/mwproject/FragmentRanking.java
+++ app/src/main/java/com/example/mwproject/FragmentRanking.java
@@ -13,6 +13,6 @@ public class FragmentRanking extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ranking, container, false);
+        return inflater.inflate(R.layout.ranking, container, false);
     }
 }
