259d723e9f47dec9060fbc7448745e04f562c4e3
kairo139 <kairo139@naver.com>
2020-06-18 11:41:00 +0900

    2020.06.17

---
+++ app/src/main/java/com/example/mwproject/passwdchange.java
@@ -0,0 +1,15 @@
+package com.example.mwproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class passwdchange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.passwdchange);
+
+    }
+}
