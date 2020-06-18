1bab7a595c6413d985005d8e5850df30028af8ba
kairo139 <kairo139@naver.com>
2020-06-18 21:11:14 +0900

    2020.06.17

--- app/src/main/java/com/example/mwproject/Player.java
+++ app/src/main/java/com/example/mwproject/Player.java
@@ -11,7 +11,6 @@ public class Player extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
-
     }
 }
 
