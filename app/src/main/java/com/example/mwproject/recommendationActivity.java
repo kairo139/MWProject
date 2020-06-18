310ad6063aad7031d0b70980b07968e6b650f6ef
wjy <wjy308@nate.com>
2020-06-18 20:49:14 +0900

    wjy

--- app/src/main/java/com/example/mwproject/recommendationActivity.java
+++ app/src/main/java/com/example/mwproject/recommendationActivity.java
@@ -1,30 +1,35 @@
 package com.example.mwproject;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class recommendationActivity extends AppCompatActivity {
-
+public class recommendationActivity extends Fragment {
     TextView reco_tvRecom1, reco_tvRecom1_1, reco_tvRecom1_2, reco_tvRecom2, reco_tvRecom2_1;
     ImageButton reco_ibRecom1_1, reco_ibRecom1_2, reco_ibRecom2_1;
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.recommendation);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View Current_v = inflater.inflate(R.layout.recommendation, container, false);
 
-        reco_tvRecom1 = findViewById(R.id.tvRecom1);
-        reco_tvRecom1_1 = findViewById(R.id.tvRecom1_1);
-        reco_tvRecom1_2 = findViewById(R.id.tvRecom1_2);
-        reco_tvRecom2 = findViewById(R.id.tvRecom2);
-        reco_tvRecom2_1 = findViewById(R.id.tvRecom2_1);
-        reco_ibRecom1_1 = findViewById(R.id.ibRecom1_1);
-        reco_ibRecom1_2 = findViewById(R.id.ibRecom1_2);
-        reco_ibRecom2_1 = findViewById(R.id.ibRecom2_1);
+
+        reco_tvRecom1 = Current_v.findViewById(R.id.tvRecom1);
+        reco_tvRecom1_1 = Current_v.findViewById(R.id.tvRecom1_1);
+        reco_tvRecom1_2 = Current_v.findViewById(R.id.tvRecom1_2);
+        reco_tvRecom2 = Current_v.findViewById(R.id.tvRecom2);
+        reco_tvRecom2_1 = Current_v.findViewById(R.id.tvRecom2_1);
+        reco_ibRecom1_1 = Current_v.findViewById(R.id.ibRecom1_1);
+        reco_ibRecom1_2 = Current_v.findViewById(R.id.ibRecom1_2);
+        reco_ibRecom2_1 = Current_v.findViewById(R.id.ibRecom2_1);
 
         reco_tvRecom1.setText("로맨스 어떠신가요?");
 
@@ -38,5 +43,8 @@ public class recommendationActivity extends AppCompatActivity {
 
         reco_ibRecom2_1.setImageResource(R.drawable.ijj1);
         reco_tvRecom2_1.setText("일진에게 찍혔을 때\n\n콕tv\n\n한장의 사진, 한순간의 실수. 지긋지긋한 스토커를 떼어내려 프사로 설정한 남친짤이 우리 반 일진 사진이라고?");
+
+        return Current_v;
     }
+
 }
