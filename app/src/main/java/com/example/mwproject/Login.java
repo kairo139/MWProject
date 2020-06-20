6b60836d5963ce5a1cda8ee97cb5c54b7a227590
wjy <wjy308@nate.com>
2020-06-21 05:03:00 +0900

    보관함 intent로 수정, 로그인 임시 id,pw로 연동했움.

---
+++ app/src/main/java/com/example/mwproject/Login.java
@@ -0,0 +1,36 @@
+package com.example.mwproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    EditText edtID, edtPW;
+    Button btnLogin,btnSignUp, btnClose;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_dialog);
+        btnLogin = findViewById(R.id.btnLogin); btnSignUp = findViewById(R.id.btnSignUp); btnClose = findViewById(R.id.cancel);
+        edtID = findViewById(R.id.edtID); edtPW = findViewById(R.id.edtPW);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(edtID);
+            }
+        });
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
