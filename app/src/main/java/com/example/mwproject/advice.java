693936b5fa2975067ab16727ce00bfe42e3b49b7
kairo139 <kairo139@naver.com>
2020-06-23 23:20:36 +0900

    2020.06.17

---
+++ app/src/main/java/com/example/mwproject/advice.java
@@ -0,0 +1,14 @@
+package com.example.mwproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class advice extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.advice);
+
+    }
+}
