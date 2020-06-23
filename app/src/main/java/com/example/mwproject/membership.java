b96c85f5b313085707766e7848e7e2ad710024a3
kairo139 <kairo139@naver.com>
2020-06-23 20:38:04 +0900

    2020.06.17

---
+++ app/src/main/java/com/example/mwproject/membership.java
@@ -0,0 +1,18 @@
+package com.example.mwproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class membership extends AppCompatActivity {
+
+    Integer[] membershipimgID = {R.drawable.membership_school_icon,R.drawable.membership_company_img,R.drawable.membership_action_img,R.drawable.membership_comedy_img,R.drawable.membership_daily_img,R.drawable.membership_romance_img,R.drawable.membership_fantasy_img,R.drawable.membership_inference_img,R.drawable.membership_reality_img,R.drawable.membership_webtoon_img};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.membership);
+
+
+    }
+}
