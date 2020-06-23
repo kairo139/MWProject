3cd9a802768ace3878cb971b052f4bc71ee88795
kairo139 <kairo139@naver.com>
2020-06-23 22:31:35 +0900

    2020.06.17

--- app/src/main/java/com/example/mwproject/membership.java
+++ app/src/main/java/com/example/mwproject/membership.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class membership extends AppCompatActivity {
 
-    Integer[] membershipimgID = {R.drawable.membership_school_icon,R.drawable.membership_company_img,R.drawable.membership_action_img,R.drawable.membership_comedy_img,R.drawable.membership_daily_img,R.drawable.membership_romance_img,R.drawable.membership_fantasy_img,R.drawable.membership_inference_img,R.drawable.membership_reality_img,R.drawable.membership_webtoon_img};
+    //Integer[] membershipimgID = {R.drawable.membership_school_icon,R.drawable.membership_company_img,R.drawable.membership_action_img,R.drawable.membership_comedy_img,R.drawable.membership_daily_img,R.drawable.membership_romance_img,R.drawable.membership_fantasy_img,R.drawable.membership_inference_img,R.drawable.membership_reality_img,R.drawable.membership_webtoon_img};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
