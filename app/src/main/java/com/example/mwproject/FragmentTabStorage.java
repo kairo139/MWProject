b4d9aa188c6e1914df1aaed2499b53836c917ca0
wjy <wjy308@nate.com>
2020-06-24 14:27:41 +0900

    ak

--- app/src/main/java/com/example/mwproject/FragmentTabStorage.java
+++ app/src/main/java/com/example/mwproject/FragmentTabStorage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +13,13 @@ import androidx.fragment.app.FragmentManager;
 
 public class FragmentTabStorage extends Fragment {
 
+    ImageButton iBtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_storage, container, false);
+        View Current_v = inflater.inflate(R.layout.tab_storage, container, false);
+
+        return Current_v;
     }
 }
