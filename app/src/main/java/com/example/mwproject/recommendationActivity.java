229e7ba00837777ed6689e02bab4fcbe483ffe9a
wjy <wjy308@nate.com>
2020-06-24 14:28:55 +0900

    ak

--- app/src/main/java/com/example/mwproject/recommendationActivity.java
+++ app/src/main/java/com/example/mwproject/recommendationActivity.java
@@ -33,15 +33,15 @@ public class recommendationActivity extends Fragment {
 
         reco_tvRecom1.setText("로맨스 어떠신가요?");
 
-        reco_ibRecom1_1.setImageResource(R.drawable.ypl2);
+        reco_ibRecom1_1.setBackgroundResource(R.drawable.ypl2);
         reco_tvRecom1_1.setText("연애플레이리스트2\n\n플레이리스트\n\n풋풋한 대학 청춘 멜로 드라마");
 
-        reco_ibRecom1_2.setImageResource(R.drawable.ypl1);
+        reco_ibRecom1_2.setBackgroundResource(R.drawable.ypl1);
         reco_tvRecom1_2.setText("연애플레이리스트1\n\n플레이리스트\n\n새학기 시작을 풋풋한 대학 청춘 멜로와 함께");
 
         reco_tvRecom2.setText("학교 좋아하시나요?");
 
-        reco_ibRecom2_1.setImageResource(R.drawable.ijj1);
+        reco_ibRecom2_1.setBackgroundResource(R.drawable.ijj1);
         reco_tvRecom2_1.setText("일진에게 찍혔을 때\n\n콕tv\n\n한장의 사진, 한순간의 실수. 지긋지긋한 스토커를 떼어내려 프사로 설정한 남친짤이 우리 반 일진 사진이라고?");
 
         return Current_v;
