a8ef18a3e2968e225f5036a5cb34e13b38835a28
wjy308 <wjy308@nate.com>
2020-10-12 15:01:04 +0900

    f

--- app/src/main/java/com/example/mwproject/Player.java
+++ app/src/main/java/com/example/mwproject/Player.java
@@ -2,10 +2,7 @@ package com.example.mwproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
