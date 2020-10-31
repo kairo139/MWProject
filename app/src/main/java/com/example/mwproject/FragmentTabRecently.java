94517d7007ee79fa5e36bd5bb3091885b3ad698a
kairo139 <kairo139@naver.com>
2020-10-31 17:51:37 +0900

    [2020.10.31.bh] 책갈피 기본연결

--- app/src/main/java/com/example/mwproject/FragmentTabRecently.java
+++ app/src/main/java/com/example/mwproject/FragmentTabRecently.java
@@ -1,9 +1,11 @@
 package com.example.mwproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +15,23 @@ import androidx.fragment.app.FragmentManager;
 import java.util.ArrayList;
 
 public class FragmentTabRecently extends Fragment{
+    View Current_v;
+    ImageButton recent_ep2;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_recently, container, false);
-}
+        Current_v = inflater.inflate(R.layout.tab_recently, container, false);
+
+        recent_ep2 = Current_v.findViewById(R.id.recent_ep2);
+        recent_ep2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Player.class);
+                intent.putExtra("playTime",80000);
+                startActivity(intent);
+            }
+        });
+                return Current_v;
+    }
 }
