6d417593787d864706acc4601575d0f9a016254d
kairo139 <kairo139@naver.com>
2020-11-04 21:53:07 +0900

    [2020.11.04.bh] player getExtra 수정

--- app/src/main/java/com/example/mwproject/DeveloperKey.java
+++ app/src/main/java/com/example/mwproject/DeveloperKey.java
@@ -1,5 +1,6 @@
 package com.example.mwproject;
 
 public class DeveloperKey {
-    public static final String DEVELOPER_KEY = "AIzaSyA6hhyRtJI53OLTgrtRmY8kCaJu0i5KZUY";
+    /*public static final String DEVELOPER_KEY = "AIzaSyA6hhyRtJI53OLTgrtRmY8kCaJu0i5KZUY";*/
+    public static final String DEVELOPER_KEY = "AIzaSyBlbdXum8Mtzp8xKZJgEDN631CL0wrC9Bc";
 }
--- app/src/main/java/com/example/mwproject/Player.java
+++ app/src/main/java/com/example/mwproject/Player.java
@@ -2,6 +2,7 @@ package com.example.mwproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.fragment.app.FragmentTransaction;
@@ -11,9 +12,13 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class Player extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
-    String vidieoID = getIntent().getStringExtra("videoID");    // 데이터 베이스 완벽히 연결 후 실행 하려는 유튜브 영상 아이디 가져오기
+public class Player extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+    String vidieoID = getIntent().getExtras().getString("videoID",""); // 데이터 베이스 완벽히 연결 후 실행 하려는 유튜브 영상 아이디 가져오기
     YouTubePlayer players;
 
     private static final int RECOVERY_DIALOG_REQUEST = 1;
@@ -35,22 +40,29 @@ public class Player extends YouTubeBaseActivity implements YouTubePlayer.OnIniti
             player.cueVideo(vidieoID,getIntent().getIntExtra("playTime",0));
         }else {
             player.cueVideo(vidieoID);
-        }/*
-                1.이부분에다가 책갈피에서 들어온거면 sqlite 확인해서 시간 가져와서 재생
-                player.seekToMillis(이부분에 가져온 시간 대입);
-                player.play();
-                2.그냥 다른 부분에서 들어온거면 처음부터 실행
-                player.cueVideo(vidieoID);
-                player.play();
-             */
-
+        }
     }
 
     // 해당하는 데이터 베이스는 SQLite Database 를 활용하여 저장할걸로 변경
     @Override
     public void onBackPressed(){    // 뒤로가기 버튼을 눌렀을 시
+
         players.pause();
-        int pauseTime = players.getCurrentTimeMillis();  // 영상을 실행 한 후 경과된 시간을 리턴
+
+/*        int User_SEQ = 1;
+        int Detail_SEQ = 1;
+        int View_TIME = players.getCurrentTimeMillis();  // 영상을 실행 한 후 경과된 시간을 리턴
+
+        String link = "http://https://mw-zhdtw.run.goorm.io/insertViewRecord.php?User_SEQ="+ User_SEQ + "&Detail_SEQ=" + Detail_SEQ + "&View_TIME=" + View_TIME;
+        try {
+            URL url = new URL(link);
+            URLConnection conn = url.openConnection();
+
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }*/
 
         /*
             insert into ViewRecord(User_id,Video_id,Date,Time) values(AI 사용해서 자동 올리기, User_ID(일단 test), Video_ID(vidieoID), sysdate() , pauseTime)
